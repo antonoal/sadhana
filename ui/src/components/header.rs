00915586ff847a2d0a13a9d15e12575a7b7011db
Alex Antonov <3091455@gmail.com>
2025-07-10 19:10:28 +0000

    Header skeleton with tailwindcss

---
+++ ui/src/components/header.rs
@@ -0,0 +1,9 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn Header() -> impl IntoView {
+    view! {
+        <div class="bg-hero dark:bg-herod bg-no-repeat bg-cover bg-center h-screen w-full fixed -z-10">
+        </div>
+    }
+}
---
+++ ui/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.rs"],
+  theme: {
+    fontFamily: {
+      sans: ["montserrat", ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      boxShadow: {
+        'inset-amber-400': '0 0 0 2px rgba(251, 191, 36, 1) inset',
+      },
+      padding: {
+        "safe-top": "env(safe-area-inset-top)"
+        // "safe-bottom": "env(safe-area-inset-bottom)",
+        // "safe-left": "env(safe-area-inset-left)",
+        // "safe-right": "env(safe-area-inset-right)",
+      },
+      future: {
+        hoverOnlyWhenSupported: true,
+      },
+      backgroundImage: {
+        hero: "linear-gradient(rgba(255, 252, 250, 0.4), #f9fafb), url('images/background.jpg')",
+        herod:
+          "linear-gradient(rgba(73, 50, 34, 0.6), #1a1631), url('images/background.jpg')"
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('can-hover', '@media (hover: hover) and (pointer: fine)')
+    })
+  ]
+};
